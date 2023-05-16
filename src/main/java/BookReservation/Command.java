0b90cc393cf6c9c7838977be0d80bc94aa14c68a
0chanchoi <118790426+0chanchoi@users.noreply.github.com>
2023-05-16 13:01:08 +0900

    BookReservation 주석추가

--- src/main/java/BookReservation/Command.java
+++ src/main/java/BookReservation/Command.java
@@ -3,3 +3,8 @@ package BookReservation;
 public interface Command {
     void execute();
 }
+//Command 인터페이스는 명령(Command) 객체들이 구현해야 하는 메서드인 execute()를 선언합니다.
+// CommandFactory 클래스는 createCommand() 메서드를 통해 ReserveCommand와 UnreserveCommand 객체를 생성합니다.
+// 이 클래스는 commandName과 Book 객체를 인자로 받으며,
+// commandName이 "Reserve"이면 ReserveCommand 객체를, "Unreserve"이면 UnreserveCommand 객체를 반환합니다.
+// 그렇지 않은 경우에는 IllegalArgumentException 예외를 발생시킵니다.
--- src/main/java/BookReservation/ReserveCommand.java
+++ src/main/java/BookReservation/ReserveCommand.java
@@ -20,3 +20,6 @@ public class ReserveCommand implements Command {
         System.out.println(book.getTitle() + "이(가) 예약되었습니다.");
     }
 }
+//ReserveCommand와 UnreserveCommand 클래스는 Command 인터페이스를 구현합니다.
+// 이 클래스들은 Book 객체를 인자로 받으며, execute() 메서드에서는 해당 도서 객체의 reserve() 또는 unreserve() 메서드를
+// 호출하여 도서를 예약 또는 취소합니다. 또한 예약 또는 취소된 도서의 제목을 출력합니다.
